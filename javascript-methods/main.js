2bf21fa46098d7617871cd2e8c287d2a236de9b6
Samuel Lee <slee0258@gmail.com>
2023-02-01 00:02:23 +0000

    Completed javascript-methods exercise.

---
+++ javascript-methods/main.js
@@ -0,0 +1,57 @@
+var five = 5;
+var twenty = 20;
+var ninetySix = 96;
+var maximumValue = Math.max(five, twenty, ninetySix);
+console.log('maximumValue:', maximumValue);
+
+var heroes = ['Spiderman', 'Batman', 'Ironman', 'Shang-Chi'];
+var randomNumber = Math.random(heroes);
+randomNumber *= heroes.length;
+var randomIndex = Math.floor(randomNumber);
+console.log('randomIndex:', randomIndex);
+
+var randomHero = heroes[randomIndex];
+console.log('randomHero:', randomHero);
+
+var library = [
+  {
+    title: "Harry Potter and the Sorcerer's Stone",
+    author: 'J.K. Rowling'
+  },
+  {
+    title: 'The Lion, the Witch, and the Wardrobe',
+    author: 'C.S. Lewis'
+  },
+  {
+    title: 'Catcher in the Rye',
+    author: 'J.D. Salinger'
+  }
+];
+var lastBook = library.pop(2);
+console.log('lastBook:', lastBook);
+
+var firstBook = library.shift(0);
+console.log('firstBook:', firstBook);
+
+var js = {
+  title: 'JavaScript for Impatient Programmers',
+  author: 'Dr. Axel Rauschmayer'
+};
+
+var css = {
+  title: 'CSS Secrets',
+  author: 'Lea Verou'
+};
+
+library.push(js);
+library.unshift(css);
+library.splice(1, 1);
+console.log('library:', library);
+
+var fullName = 'Sam Lee';
+var firstAndLastName = fullName.split(' ');
+console.log('firstAndLastName:', firstAndLastName);
+
+var firstName = firstAndLastName[0];
+var sayMyName = firstName.toUpperCase();
+console.log('sayMyName:', sayMyName);
