210cc129c1f5875801a0bc6b25e9e1114994ffe4
Samuel Lee <slee0258@gmail.com>
2023-02-07 17:55:07 +0000

    In progress.

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,1 +1,17 @@
 /* exported chunk */
+/*
+  - define a function named chunk with two parameters "array" and "size"
+  - assign an empty array to a variable named chunkArr
+  - create a for loop to loop through the given array
+    - if the array.length is strictly equal to the size
+      - push to chunkArr
+  - return chunkArr
+*/
+
+function chunk(array, size) {
+  var chunkArr = [];
+  for (var index = 0; index < array.length; index += size) {
+    chunkArr.push(array.slice(index, index + size));
+  }
+  return chunkArr;
+}
--- array-manipulation-2/src/drop.js
+++ array-manipulation-2/src/drop.js
@@ -1,5 +1,19 @@
 /* exported drop */
 /*
   - define a function named drop with two parameters "array" and "count"
-
+  - assign an empty array to a variable named droppedArr
+  - create a for loop to loop through the given array
+    - if index number is greater than or equal to count
+      - push the element at the index number into droppedArr
+  - return droppedArr
 */
+
+function drop(array, count) {
+  var droppedArr = [];
+  for (var index = 0; index < array.length; index++) {
+    if (index >= count) {
+      droppedArr.push(array[index]);
+    }
+  }
+  return droppedArr;
+}
