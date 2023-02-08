228ee393a7f0755ef85a778d82a7ad4e6ff98691
Samuel Lee <slee0258@gmail.com>
2023-02-08 20:09:38 +0000

    Moved the variables to local scope of the handleSubmit function and instead of getting the value of the queried elements got the value from the .elements.property.value instead, according to step 11 in the exercise.

--- javascript-forms/form-events.js
+++ javascript-forms/form-events.js
@@ -9,7 +9,7 @@ function handleBlur(event) {
 }
 
 function handleInput(event) {
-  console.log('value of name:', event.target.value);
+  console.log('value of', event.target.name + ':' + event.target.value);
 }
 
 var $name = document.querySelector('#user-name');
--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -1,14 +1,14 @@
-var $name = document.querySelector('#user-name');
-var $email = document.querySelector('#user-email');
-var $message = document.querySelector('#user-message');
 var $form = document.querySelector('#contact-form');
 
 function handleSubmit(event) {
   event.preventDefault();
+  var name = $form.elements.name.value;
+  var email = $form.elements.email.value;
+  var message = $form.elements.message.value;
   var messageData = {
-    $name: $name.value,
-    $email: $email.value,
-    $message: $message.value
+    name,
+    email,
+    message
   };
   console.log('messageData:', messageData);
   $form.reset();
