2971498adeeec59d641a9aa63dd19869f570fac3
Samuel Lee <slee0258@gmail.com>
2023-02-08 00:16:57 +0000

    Completed array-manipulation-2 practice problems.

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -3,8 +3,11 @@
   - define a function named chunk with two parameters "array" and "size"
   - assign an empty array to a variable named chunkArr
   - create a for loop to loop through the given array
-    - if the array.length is strictly equal to the size
-      - push to chunkArr
+    - initialize the loop at index 0; check if index is less than array.length; final expression
+      is index added to size and reassigned to index
+        - using the slice method slice the array at the start of the index and end
+          at the result of the expression index plus size
+        - push the sliced array to chunkArr
   - return chunkArr
 */
 
