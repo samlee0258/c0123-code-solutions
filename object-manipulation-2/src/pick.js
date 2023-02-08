6b90994ae26dbf04b98354851ee3bc93a393a524
Samuel Lee <slee0258@gmail.com>
2023-02-08 08:14:17 +0000

    In progress.

---
+++ object-manipulation-2/src/pick.js
@@ -0,0 +1,9 @@
+/* exported pick */
+/*
+  - define a function named pick with two parameters "source" and "keys"
+  - assign an empty object to a variable
+  - create a for in loop to loop through the object
+    - create a for loop to loop through the keys array
+      - if the key in object is strictly equal to the index in array
+
+*/
