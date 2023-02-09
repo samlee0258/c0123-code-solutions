108382e40cae8863edd6a53bce5e40eafff593bd
Samuel Lee <slee0258@gmail.com>
2023-02-09 22:07:28 +0000

    Completed dom-event-delegation exercise.

---
+++ dom-event-delegation/main.js
@@ -0,0 +1,13 @@
+var $taskList = document.querySelector('.task-list');
+
+$taskList.addEventListener('click', function (event) {
+
+  console.log('event.target:', event.target);
+  console.log('event.target.tagName:', event.target.tagName);
+
+  if (event.target.tagName === 'BUTTON') {
+    var $closestLi = event.target.closest('.task-list-item');
+    console.log('closest .task-list-item:', $closestLi);
+    $closestLi.remove();
+  }
+});
