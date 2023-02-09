8e81721dd66e69a5ad19d33769ed1887a2c67b90
Samuel Lee <slee0258@gmail.com>
2023-02-09 07:32:42 +0000

    In progress.

--- object-manipulation-2/src/pick.js
+++ object-manipulation-2/src/pick.js
@@ -4,6 +4,19 @@
   - assign an empty object to a variable
   - create a for in loop to loop through the object
     - create a for loop to loop through the keys array
-      - if the key in object is strictly equal to the index in array
-
+      - if the key in object is strictly equal to the index in array push the key
+        and the value into the empty object
+  - return output object
 */
+
+function pick(source, keys) {
+  var presentKeys = {};
+  for (var key in source) {
+    for (var index = 0; index < keys.length; index++) {
+      if (key === keys[index]) {
+        presentKeys = key;
+      }
+    }
+  }
+  return presentKeys;
+}
