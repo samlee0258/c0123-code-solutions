261ad4a7ba9960703986c0e3a20174ff431de602
Samuel Lee <slee0258@gmail.com>
2023-02-15 07:38:26 +0000

    Completed object-manipulation-2 practice problems.

--- object-manipulation-2/src/defaults.js
+++ object-manipulation-2/src/defaults.js
@@ -2,11 +2,16 @@
 /*
   - get "bar" from source and place it inside target object
   - if "foo" and "baz" exists in target ignore in source
+
+  - define a function named defaults with two parameters "target" and "source"
+  - create a for in loop to loop through the source object
+    - if the target at key is strictly equal to undefined
+      - assign source at key to target at key
 */
 
 function defaults(target, source) {
   for (var key in source) {
-    if (target[key] !== source[key]) {
+    if (target[key] === undefined) {
       target[key] = source[key];
     }
   }
