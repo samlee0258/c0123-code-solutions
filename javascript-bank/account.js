765682ed7e245e828f0a5f24a444ab411bbb9461
Samuel Lee <slee0258@gmail.com>
2023-02-23 18:50:10 +0000

    In progress of the bank portion of the challenge.

--- javascript-bank/account.js
+++ javascript-bank/account.js
@@ -26,5 +26,17 @@ Account.prototype.withdraw = function (amount) {
 };
 
 Account.prototype.getBalance = function () {
-
+  var totalDeposit = 0;
+  var totalWithdrawal = 0;
+  for (var index = 0; index < this.transactions.length; index++) {
+    if (this.transactions.length < 0) {
+      return 0;
+    } else if (this.transactions[index].type === 'deposit') {
+      totalDeposit += this.transactions[index].amount;
+    } else if (this.transactions[index].type === 'withdrawal') {
+      totalWithdrawal += this.transactions[index].amount;
+    }
+  }
+  var balance = totalDeposit - totalWithdrawal;
+  return balance;
 };
--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -1,1 +1,13 @@
 /* exported Bank */
+function Bank() {
+  this.nextAccountNumber = 1;
+  this.accounts = [];
+}
+
+// Bank.prototype.openAccount = function (holder, balance) {
+//   if (!Number.isInteger(balance) || balance <= 0) {
+//     return null;
+//   } else {
+
+//   }
+// };
