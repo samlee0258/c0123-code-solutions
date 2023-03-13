bb1aaac26cbbbf9e3638dc9430b025cff69a4a64
Samuel Lee <slee0258@gmail.com>
2023-03-13 16:23:55 +0000

    In progress.

--- string-manipulation-3/src/is-anagram.js
+++ string-manipulation-3/src/is-anagram.js
@@ -1,1 +1,23 @@
 /* exported isAnagram */
+/*
+  - put strings into objects to see if the letter values are the same
+  - remove spaces from both strings
+  - loop through both strings and compare them
+  - if the characters exist in both strings return true
+*/
+
+function isAnagram(firstString, secondString) {
+  firstString = firstString.split(' ').join('');
+  secondString = secondString.split(' ').join('');
+  var equal = null;
+  for (var i = 0; i < firstString.length; i++) {
+    for (var index = 0; index < secondString.length; index++) {
+      if (firstString[i] !== secondString[index]) {
+        equal = false;
+      } else {
+        equal = true;
+      }
+    }
+  }
+  return equal;
+}
--- string-manipulation-3/src/is-palindromic.js
+++ string-manipulation-3/src/is-palindromic.js
@@ -1,10 +1,22 @@
 /* exported isPalindromic */
 /*
-  -
+  - assign an empty string to a variable
+  - removing spacing in string
+  - take string and reverse the order of the characters and join them together
+  - assign reversed string to the variable
+  - compare the reversed string to the string
+  - if they not strictly equal to each other return false
+  - otherwise return true
 */
 
-// function isPalindromic(string) {
-//   if ('string' === 'string') {
-//     return true;
-//   }
-// }
+function isPalindromic(string) {
+  var reversedStr = '';
+  string = string.split(' ').join('');
+  for (var index = string.length - 1; index >= 0; index--) {
+    reversedStr += string[index];
+  }
+  if (reversedStr !== string) {
+    return false;
+  }
+  return true;
+}
