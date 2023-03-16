6bb8ac81a6e7896d67b326d84d11fad90ea8e86c
Samuel Lee <slee0258@gmail.com>
2023-03-16 08:37:14 +0000

    In progress of working on the balance problem of the exercise.

---
+++ array-reduce/main.js
@@ -0,0 +1,30 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const account = [
+  { type: 'deposit', amount: 150 },
+  { type: 'deposit', amount: 20 },
+  { type: 'withdrawal', amount: 5 },
+  { type: 'deposit', amount: 100 },
+  { type: 'withdrawal', amount: 25 },
+  { type: 'withdrawal', amount: 60 },
+];
+
+const traits = [
+  { color: 'yellow' },
+  { type: 'electric' },
+  { name: 'pikachu' },
+  { level: 15 },
+  { trainer: 'ash' },
+];
+
+const initialValue = 0;
+const sum = numbers.reduce(
+  (num1, num2) => num1 + num2, initialValue
+);
+console.log('sum:', sum);
+
+const startingNum = 1;
+const product = numbers.reduce(
+  (num1, num2) => num1 * num2, startingNum
+);
+console.log('product:', product);
