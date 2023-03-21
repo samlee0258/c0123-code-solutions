cf487055ed09dff9eabbeea6b24410716c81e9ff
Samuel Lee <slee0258@gmail.com>
2023-03-21 19:06:55 +0000

    In progres.

---
+++ express-delete/server.js
@@ -0,0 +1,47 @@
+import express from 'express';
+
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
+const app = express();
+
+app.get('/api/grades', (req, res) => {
+  const gradesArr = [];
+  for (const grade in grades) {
+    gradesArr.push(grades[grade]);
+  }
+  res.json(gradesArr);
+});
+
+app.delete('/api/grades/:id', (req, res) => {
+  const id = req.params.id;
+  console.log(grades.id);
+  if (id === grades.id) {
+    res.sendStatus(204);
+    // delete grades.id;
+  } else {
+    res.sendStatus(404);
+  }
+});
+
+app.listen(8080, () => {
+  console.log('Listening on port 8080!');
+});
