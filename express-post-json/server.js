79ae3fda674e014c5b1b0cd771c45e4439c1ac06
Samuel Lee <slee0258@gmail.com>
2023-03-21 22:43:09 +0000

    Completed express-post-json exercise.

--- express-post-json/server.js
+++ express-post-json/server.js
@@ -1,6 +1,28 @@
-// import express from 'express';
+import express from 'express';
 
-// const grades = {};
+const app = express();
 
-// const app = express();
-console.log('hello');
+app.use(express.json());
+
+const grades = {};
+let nextId = 1;
+
+app.get('/api/grades', (req, res) => {
+  const gradesArr = [];
+  for (const id in grades) {
+    gradesArr.push(grades[id]);
+  }
+  res.json(gradesArr);
+});
+
+app.post('/api/grades', (req, res) => {
+  const newGrade = req.body;
+  const id = nextId++;
+  newGrade.id = id;
+  grades[id] = newGrade;
+  res.status(201).json(newGrade);
+});
+
+app.listen(8080, () => {
+  console.log('Listening on port 8080!');
+});
