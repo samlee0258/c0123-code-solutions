1dbed57166218b8d908f9033dc937eda1787c46e
Samuel Lee <slee0258@gmail.com>
2023-03-24 06:46:30 +0000

    Finished writing the GET endpoint.

--- express-notes-api/server.js
+++ express-notes-api/server.js
@@ -1,18 +1,23 @@
-import { data } from './data.json';
+import { readFileSync } from 'node:fs';
 import express from 'express';
-
+const port = 8080;
 const app = express();
 
 app.use(express.json());
 
+const readFile = () => {
+  const data = readFileSync('data.json', 'utf8');
+  const parsedData = JSON.parse(data);
+  return parsedData;
+};
+
 app.get('/api/notes', (req, res) => {
+  const parsedData = readFile();
   const notesArr = [];
-  for (const key in data.notes) {
-    notesArr.push((data[key]));
+  for (const key in parsedData.notes) {
+    notesArr.push((parsedData.notes[key]));
   }
   res.json(notesArr);
 });
 
-app.listen(8080, () => {
-  console.log('Listening on port 8080!');
-});
+app.listen(port, () => console.log(`Listening on port: ${port}!`));
