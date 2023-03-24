25005f6177f21e8cacb5f15f063b89ece4ba6ea9
Samuel Lee <slee0258@gmail.com>
2023-03-24 07:36:47 +0000

    Finished GET request for individual note.

--- express-notes-api/server.js
+++ express-notes-api/server.js
@@ -11,6 +11,20 @@ const readFile = () => {
   return parsedData;
 };
 
+app.get('/api/notes/:id', (req, res) => {
+  const id = Number(req.params.id);
+  console.log('isInteger:', !Number.isInteger(id));
+  if (id < 0 || !Number.isInteger(id)) {
+    return res.status(400).json({ error: 'Must be a positive integer!' });
+  }
+  const parsedData = readFile();
+  console.log('parsedData:', parsedData);
+  if (!parsedData.notes[id]) {
+    return res.status(404).json({ error: 'Note does not exist!' });
+  }
+  res.status(200).json(parsedData.notes[id]);
+});
+
 app.get('/api/notes', (req, res) => {
   const parsedData = readFile();
   const notesArr = [];
