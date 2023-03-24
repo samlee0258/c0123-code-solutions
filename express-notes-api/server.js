4b20c2cf4bf3773ea96cad98353918ae67dad2cd
Samuel Lee <slee0258@gmail.com>
2023-03-24 08:13:46 +0000

    Added notes on what to do for add new note, delete note, and update note.

--- express-notes-api/server.js
+++ express-notes-api/server.js
@@ -1,5 +1,7 @@
 import { readFileSync } from 'node:fs';
+// import { writeFile } from 'node:fs/promises';
 import express from 'express';
+
 const port = 8080;
 const app = express();
 
@@ -11,6 +13,7 @@ const readFile = () => {
   return parsedData;
 };
 
+// Get individual note:
 app.get('/api/notes/:id', (req, res) => {
   const id = Number(req.params.id);
   console.log('isInteger:', !Number.isInteger(id));
@@ -24,7 +27,7 @@ app.get('/api/notes/:id', (req, res) => {
   }
   res.status(200).json(parsedData.notes[id]);
 });
-
+// Get all notes:
 app.get('/api/notes', (req, res) => {
   const parsedData = readFile();
   const notesArr = [];
@@ -34,4 +37,21 @@ app.get('/api/notes', (req, res) => {
   res.json(notesArr);
 });
 
+// Add new note:
+// POST to /api/notes
+// req.body is whatever text comes after the endpoint and is turned into an object
+// req.body.content = whatever you type if you do (content="")
+// 3. write a writeFile function similar to readFile function as a promise
+//   - if succeeds post json
+//   - else show 500
+
+// Delete note:
+// step 3 and 4 can be a Promise using await .then would be the success and .catch would be the error.
+// step 1 and 2 is similar to getting individual note steps 1 and 2
+
+// Update note:
+// PUT request to endpoint /api/notes/:id
+// if not positive integer or no req.body content property throw error 400
+// if id and content doesnt match note id throw error 404
+// step 3 and 4 can be a Promise using await similar to Delete
 app.listen(port, () => console.log(`Listening on port: ${port}!`));
