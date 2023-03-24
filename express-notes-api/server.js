0f37714b88e073563767a0e2aefa2f851d1ea44f
Samuel Lee <slee0258@gmail.com>
2023-03-24 22:33:48 +0000

    Finished POST and used try / catch as well as made async / await for promises.

--- express-notes-api/server.js
+++ express-notes-api/server.js
@@ -1,27 +1,33 @@
-import { readFileSync } from 'node:fs';
-// import { writeFile } from 'node:fs/promises';
+import { readFile, writeFile } from 'node:fs/promises';
 import express from 'express';
 
 const port = 8080;
 const app = express();
-
 app.use(express.json());
 
-const readFile = () => {
-  const data = readFileSync('data.json', 'utf8');
-  const parsedData = JSON.parse(data);
-  return parsedData;
+const read = async () => {
+  try {
+    const data = await readFile('data.json', 'utf8');
+    const parsedData = JSON.parse(data);
+    return parsedData;
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
 };
 
+const write = async (parsedData) => {
+  await writeFile('data.json', JSON.stringify(parsedData, null, 2));
+};
 // Get individual note:
 app.get('/api/notes/:id', (req, res) => {
   const id = Number(req.params.id);
+  const parsedData = read();
   console.log('isInteger:', !Number.isInteger(id));
+  console.log('parsedData:', parsedData);
   if (id < 0 || !Number.isInteger(id)) {
     return res.status(400).json({ error: 'Must be a positive integer!' });
   }
-  const parsedData = readFile();
-  console.log('parsedData:', parsedData);
   if (!parsedData.notes[id]) {
     return res.status(404).json({ error: `Note ${id} does not exist!` });
   }
@@ -29,10 +35,10 @@ app.get('/api/notes/:id', (req, res) => {
 });
 // Get all notes:
 app.get('/api/notes', (req, res) => {
-  const parsedData = readFile();
+  const parsedData = read();
   const notesArr = [];
   for (const key in parsedData.notes) {
-    notesArr.push((parsedData.notes[key]));
+    notesArr.push(parsedData.notes[key]);
   }
   res.json(notesArr);
 });
@@ -44,6 +50,24 @@ app.get('/api/notes', (req, res) => {
 // 3. write a writeFile function similar to readFile function as a promise
 //   - if succeeds post json
 //   - else show 500
+
+app.post('/api/notes', async (req, res) => {
+  try {
+    const parsedData = await read();
+    const text = req.body;
+    if (!Object.hasOwn(text, 'content')) {
+      return res.status(400).json({ error: 'content is a required field' });
+    }
+    parsedData.notes[parsedData.nextId] = text;
+    parsedData.notes[parsedData.nextId].id = parsedData.nextId;
+    parsedData.nextId++;
+    await write(parsedData);
+    res.status(200).json(text);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'An unexpected error occurred.' });
+  }
+});
 
 // Delete note:
 // step 3 and 4 can be a Promise using await .then would be the success and .catch would be the error.
