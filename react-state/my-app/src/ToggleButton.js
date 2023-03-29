fcf48a85e7bcd60db19bc75b85dc128bf129a353
Samuel Lee <slee0258@gmail.com>
2023-03-29 17:16:19 +0000

    Made small changes to styling and added in missing semi-colons.

--- react-state/my-app/src/ToggleButton.js
+++ react-state/my-app/src/ToggleButton.js
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 
 export default function ToggleButton({text, color}) {
-  const [isClicked, setIsClicked] = useState(false)
+  const [isClicked, setIsClicked] = useState(false);
 
-  console.log('useState:', isClicked); //false on mount; true on re-render
+  console.log('useState:', isClicked); // false on mount; true on re-render
   function handleClick() {
-    console.log('before setter:', isClicked); //false
+    console.log('before setter:', isClicked); // false
     setIsClicked(!isClicked);
-    console.log('after setter:', isClicked); //false because the data referencing the state hasn't updated.
+    console.log('after setter:', isClicked); // false because the data referencing the state hasn't updated.
   }
   // either returns on line 14 or 22 re-renders the ToggleButton component, re-running the code.
   if (!isClicked) {
@@ -26,5 +26,5 @@ export default function ToggleButton({text, color}) {
     >
         {text}
     </button>
-  )
+  );
 }
