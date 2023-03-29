6d75770282de67aff0ff0158a6b1c62d04aea546
Samuel Lee <slee0258@gmail.com>
2023-03-29 06:29:58 +0000

    Completed react-state exercise.

---
+++ react-state/my-app/src/App.js
@@ -0,0 +1,13 @@
+import ToggleButton from "./ToggleButton.js";
+
+function App() {
+  return (
+    <div>
+      <ToggleButton text="Btn 1" color="red"/>
+      <ToggleButton text="Btn 2" color="green"/>
+      <ToggleButton text="Btn 3" color="blue"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-state/my-app/src/ToggleButton.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+
+export default function ToggleButton({text, color}) {
+  const [isClicked, setIsClicked] = useState(false)
+
+  console.log('useState:', isClicked); //false on mount; true on re-render
+  function handleClick() {
+    console.log('before setter:', isClicked); //false
+    setIsClicked(!isClicked);
+    console.log('after setter:', isClicked); //false because the data referencing the state hasn't updated.
+  }
+  // either returns on line 14 or 22 re-renders the ToggleButton component, re-running the code.
+  if (!isClicked) {
+    return <button
+              color="white"
+              onClick={ handleClick }
+           >
+              {text}
+           </button>;
+  }
+
+  return (
+    <button
+        style={{ backgroundColor: color }}
+        onClick={ handleClick }
+    >
+        {text}
+    </button>
+  )
+}
