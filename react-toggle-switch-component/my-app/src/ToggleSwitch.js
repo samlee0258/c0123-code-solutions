6f1d681d71345cfdbb6e58129eb63f983230d767
Samuel Lee <slee0258@gmail.com>
2023-04-11 18:59:00 +0000

    In progress.

--- react-toggle-switch-component/my-app/src/ToggleSwitch.js
+++ react-toggle-switch-component/my-app/src/ToggleSwitch.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import './App.css';
 import './ToggleSwitch.css';
 
 export default function ToggleSwitch() {
 
   return (
-    <>
+    <div className='App-header'>
       <label className='switch'>
         <input type="checkbox" />
-        <span class="slider round"></span>
+        <span class="slider round" />
       </label>
-    </>
+    </div>
   );
 }
