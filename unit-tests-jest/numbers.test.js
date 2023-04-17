dc3ceeebc7be92af049119b1e65139a3b44f4184
Samuel Lee <slee0258@gmail.com>
2023-04-17 18:00:16 +0000

    In progress.

---
+++ unit-tests-jest/numbers.test.js
@@ -0,0 +1,14 @@
+// import { evenNumbers, toDollars, divideBy, multiplyBy } from './numbers.js';
+
+// describe('evenNumbers()', () => {
+
+//   it('returns the even numbers', () => {
+//     const numbers = [1, 4, 5, 10, 0];
+//     const result = evenNumbers(numbers);
+//     expect(result).toEqual([4, 10, 0]);
+//   });
+//   it('works with an empty array', () => {
+
+//   });
+
+// });
